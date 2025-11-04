e0fad8390f11a12788a805c221aa47c230996213
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-11-04 20:58:41 +0100

    feat: use mupdf in example

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
 #include <QLabel>
 #include <QScrollArea>
 #include <QVBoxLayout>
@@ -18,7 +19,27 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     QWidget *container = new QWidget();
     QVBoxLayout *layout = new QVBoxLayout(container);
 
-    for (int i = 0; i < 10; ++i) {
+    fz_context *ctx = fz_new_context(NULL, NULL, FZ_STORE_UNLIMITED);
+    fz_document *doc = NULL;
+    int n_pages = 0;
+
+    std::string fileName =
+        QFileDialog::getOpenFileName(this, "Open PDF", "", "PDF Files (*.pdf);;All Files (*)")
+            .toStdString();
+
+    fz_try(ctx)
+    {
+        fz_register_document_handlers(ctx);
+        doc = fz_open_document(ctx, fileName.c_str());
+
+        n_pages = fz_count_pages(ctx, doc);
+    }
+    fz_always(ctx) { fz_drop_document(ctx, doc); }
+    fz_catch(ctx) { fprintf(stderr, "Error: %s\n", fz_caught_message(ctx)); }
+
+    fz_drop_context(ctx);
+
+    for (int i = 0; i < n_pages; ++i) {
         QLabel *label = new QLabel(QString("Label %1").arg(i + 1));
         layout->addWidget(label);
     }
