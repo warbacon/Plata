0aa133a99bc02f4087daa359d7f0fc24d68100b5
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-11-04 20:39:53 +0100

    feat: better startpage example

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,15 +1,29 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QLabel>
 #include <QScrollArea>
-#include <qnamespace.h>
-#include <qscrollarea.h>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <mupdf/fitz.h>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     setWindowTitle("Plata");
-    QScrollArea *scrollArea = new QScrollArea();
+
+    QScrollArea *scrollArea = new QScrollArea(this);
     scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+    QWidget *container = new QWidget();
+    QVBoxLayout *layout = new QVBoxLayout(container);
+
+    for (int i = 0; i < 10; ++i) {
+        QLabel *label = new QLabel(QString("Label %1").arg(i + 1));
+        layout->addWidget(label);
+    }
+
+    scrollArea->setWidget(container);
     setCentralWidget(scrollArea);
 }
 
